d99deda86a3c445d933618f880318bb3aec24c74
Ubuntu <ubuntu@ip-172-31-47-111.ap-northeast-1.compute.internal>
2022-02-10 07:26:12 +0000

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'static_pages#home'
+  get 'users/new'
+  get  '/help',    to: 'static_pages#help'
+  get  '/contact', to: 'static_pages#contact'
+  get  './sign_up', to: 'users#new'
 end
