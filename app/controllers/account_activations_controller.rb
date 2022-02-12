e10faaae6c24b4ed71ba6f0c6bca6e68d6d7456c
Ubuntu <ubuntu@ip-172-31-47-111.ap-northeast-1.compute.internal>
2022-02-12 02:53:56 +0000

    account activated editing

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -2,10 +2,10 @@ class AccountActivationsController < ApplicationController
     
     def edit 
         user = User.find_by(email: params[:email])
-        if user && !user.activation? && user.authenticated?(:activation, params[:id])
+        if user && !user.activated? && user.authenticated?(:activation, params[:id])
             user.activate
             log_in user 
-            activated_at
+            flash[:success] = "Account activated!"
             redirect_to user
         else
             flash[:danger] = "Invalid activation link"
